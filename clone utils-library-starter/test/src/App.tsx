3c9317cf390d5f0a664b791fefb1cb99e6502b1c
KimWooHyun <ktkwhms3@gmail.com>
2024-01-23 01:45:18 +0900

    Feat:  모듈에 타입스크립트, 노드 타입 추가
    1.  타입스크립트, 노드 타입 추가
    2. dist 파일 추가

--- clone utils-library-starter/test/src/App.tsx
+++ clone utils-library-starter/test/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-// import { add } from "woo1031-utils";
+import { add } from "woo1031-utils";
 import "./App.css";
 
 function App(): JSX.Element {
