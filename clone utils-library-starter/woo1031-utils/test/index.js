4c6e6abbf85d683022c6fb2520ec33b51e3af4ef
KimWooHyun <ktkwhms3@gmail.com>
2024-01-22 21:53:49 +0900

    Feat: 라이브러리 테스트 폴더 생성

---
+++ clone utils-library-starter/woo1031-utils/test/index.js
@@ -0,0 +1,6 @@
+const { add, divide, multiply, subtract } = require("woo1031-utils");
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
